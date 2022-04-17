21ae7e932f5b61af0fa53d709c3f958eb817dc9d
LUYANG23 <a2451593961@163.com>
2022-04-17 20:50:22 +0800

    hot-fix commit3

--- src/main/java/com/meng/controller/Gittest.java
+++ src/main/java/com/meng/controller/Gittest.java
@@ -7,6 +7,7 @@ public class Gittest {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git4");
+        System.out.println("hello git4");
 
     }
 }
